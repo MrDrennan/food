f91f38531aadf4cb9d406da0fad98e3c5ba76775
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-01-22 13:08:11 -0500

    added fat free and home page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Chad Drennan
+ * Date 1/15/20
+ * @link /328/chicken/index.php
+ */
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+require("vendor/autoload.php");
+
+// :: means to invoke a static method. -> means to use an instance method
+$f3 = Base::instance();
+
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run F3
+$f3->run();
